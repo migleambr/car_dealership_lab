2e52f966ea5f07eb5ecca7a7d237c8d6416d9bb1
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-09-22 21:55:04 +0300

    created Car class

---
+++ car_dealership.js
@@ -0,0 +1,23 @@
+// create a Car class with properties:
+    // manufacturer
+    // price
+    // engine type
+
+const Car = function (manufacturer, price, engineType) {
+    let _manufacturer = manufacturer;
+    let _price = price;
+    let _engineType = engineType;
+
+    this.getManufacturer = () => _manufacturer;
+    this.setManufacturer = (manufacturer) => _manufacturer = manufacturer;
+    this.getPrice = () => _price;
+    this.setPrice = (price) => _price = price;
+    this.getEngineType = () => _engineType;
+    this.setEngineType = (engineType) => _engineType = engineType;
+}
+
+// const lamborghiniUrus = new Car('Lamborghini', 222000, 'Petrol');
+// console.log(lamborghiniUrus.getManufacturer(), lamborghiniUrus.getPrice(), lamborghiniUrus.getEngineType());
+
+
+
