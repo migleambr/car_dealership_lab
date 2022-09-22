70a029ae3a1d6e1784de87cc7c5d277e39188e9a
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-09-23 00:36:35 +0300

    wrote MVP functions for Dealership prototype

--- car_dealership.js
+++ car_dealership.js
@@ -30,9 +30,74 @@ const Dealership = function (title) {
     this.setTitle = (title) => _title = title;
     this.getMaxNumberOfCars = () => _maxNumberOfCars;
     this.getStock = () => _stock;
-    this.addStock = (car) => _stock.push(car);
+    // this.addStock = (car) => _stock.push(car);
     this.removeStock = (car) => _stock.splice(_stock.indexOf(car), 1);
 }
+
+// method to count the number of cars in stock
+Dealership.prototype.countStock = function () {
+    return this.getStock().length;
+}
+
+const myDealership = new Dealership("Migle's Dealership");
+console.log(myDealership.countStock());
+
+// method to add a car to stock
+Dealership.prototype.addStock = function (Car) {
+    this.getStock().push(Car);
+}
+
+const McLaren720S = new Car("McLaren", 315000, "Petrol");
+const lamborghiniUrus = new Car("Lamborghini", 222000, "Petrol");
+const lamborghiniAventador = new Car("Lamborghini", 390000, "Petrol");
+
+myDealership.addStock(McLaren720S);
+myDealership.addStock(lamborghiniUrus);
+myDealership.addStock(lamborghiniAventador);
+console.log(myDealership.countStock());
+
+// method to return an array containing each car's manufacturer
+Dealership.prototype.allManufacturers = function () {
+    let manufacturers = [];
+    for (i = 0; i < this.countStock(); i++) {
+        manufacturers.push(this.getStock()[i].getManufacturer());
+    }
+    return manufacturers;
+} 
+
+console.log(myDealership.allManufacturers());
+
+// method to find all cars from a given manufacturer
+Dealership.prototype.carsByManufacturer = function (manufacturer) {
+    this.manufacturer = manufacturer;
+    let allCars = [];
+    // let car = [];
+    for (i = 0; i < this.countStock(); i++) {
+        if (manufacturer === this.getStock()[i].getManufacturer()) {
+            allCars.push(this.getStock()[i]);        
+            // car.push(this.getStock()[i].getManufacturer());
+            // car.push(this.getStock()[i].getPrice()); 
+            // car.push(this.getStock()[i].getEngineType());
+        }
+    }
+    // return car;
+    return allCars;
+}
+
+// console.log(myDealership.getStock()[0].getManufacturer(), myDealership.getStock()[0].getPrice(), myDealership.getStock()[0].getEngineType());
+console.log(myDealership.carsByManufacturer("Lamborghini"));
+
+
+// method to find the total value of all the cars in stock
+Dealership.prototype.totalValue = function () {
+    let value = 0;
+    for (i = 0; i < this.countStock(); i++) {
+        value += this.getStock()[i].getPrice();
+    }
+    return value;
+}
+
+console.log(myDealership.totalValue());
 
 module.exports = {Car, Dealership}; 
 
